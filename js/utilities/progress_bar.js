c8694af92879eca3f29a0754c498fade65b9d488
Henrik Bakke <119268924+Bakek-lang@users.noreply.github.com>
2024-01-20 20:14:47 +0100

    progress bar animated

--- js/utilities/progress_bar.js
+++ js/utilities/progress_bar.js
@@ -1,9 +1,37 @@
-import { makeApiCall } from "../api/fetch";
+import { makeApiCall } from "../api/fetch.js";
 const url =
   "http://code-corner.henrikbakke.no/wp-json/wp/v2/posts?acf_format=standard";
 
-let data = makeApiCall(url);
+let data = await makeApiCall(url);
 
-console.log(data);
+console.log(data[0].acf);
 
 // Make function to get categories from api, loop through and see how many posts has each of the tag
+
+function animatedProgressBar(bar, width, delay) {
+  setTimeout(() => {
+    bar.style.width = width;
+  }, delay);
+}
+
+function animatedBars() {
+  const progressBars = document.querySelectorAll(".progress");
+  const animationDuration = 1500;
+
+  progressBars.forEach((bar, index) => {
+    const width = bar.parentElement.getAttribute("data-progress");
+    const delay = index * 700;
+
+    animatedProgressBar(bar, width, delay);
+
+    const totalDelay = delay + animationDuration;
+
+    setTimeout(() => {
+      const tagProgress = bar.closest(".tag-progress");
+      const percentage = tagProgress.querySelector(".percentage");
+      percentage.style.opacity = 1;
+    }, totalDelay);
+  });
+}
+
+animatedBars();
