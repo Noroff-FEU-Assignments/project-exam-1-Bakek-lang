3028fdd47f8c5a76678c02f030767f28936a7bb9
Henrik Bakke <119268924+Bakek-lang@users.noreply.github.com>
2024-02-13 17:44:52 +0100

    aria

--- js/snippets/is_integer.js
+++
@@ -1,8 +0,0 @@
-export function isInteger(number) {
-  if (Number.isInteger(number)) {
-    number = number.toFixed(0);
-  } else {
-    number = number.toFixed(1);
-  }
-  return number;
-}
--- js/ui/loading_indicator.js
+++
@@ -1,6 +0,0 @@
-export function loadingIndicatorOff() {
-  const loadingIndicator = document.querySelectorAll(".lds-ring");
-  loadingIndicator.forEach(function (i) {
-    i.style.display = "none";
-  });
-}
